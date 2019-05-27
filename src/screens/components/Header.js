9d3755920df8f0eb992989ff85b53a886d85b118
Benjamin Leff <benjamin.w.leff@gmail.com>
2019-05-27 16:42:30 -0400

    leaving react default logo.svg until lauren gets hers

--- src/screens/components/Header.js
+++ src/screens/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import logo from '@/assets/images/logo.png'
+import logo from '@/assets/images/logo.svg'
 
 const StyledHeader = styled.header`
   width: 100%;
