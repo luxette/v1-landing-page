9dcc8eedbc8406e927a2b2d75ec62f4640b834e1
Lucas Gomes <lucasgomes.dky@gmail.com>
2019-06-03 12:32:23 -0300

    API URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const API_HOST = 'http://localhost:3006/'
+const API_HOST = 'https://luxete-api-3z4w5ilef.now.sh/'
 
 export const saveContact = values => axios.post(`${API_HOST}add-contact`, values)
